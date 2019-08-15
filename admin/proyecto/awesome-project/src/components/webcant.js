6e2f45bfb6c6f3bb0b8c96ecd5c081946558628e
brayan <brayanminavernaza19@gmail.com>
2019-08-15 15:31:18 +0000

    correccion de la camara

--- admin/proyecto/awesome-project/src/components/webcant.js
+++ admin/proyecto/awesome-project/src/components/webcant.js
@@ -1,23 +1,19 @@
-import React, { Component } from "react";
-import { AFrameRenderer, Marker } from "../src";
+import React from 'react'
+import {AFrameRenderer,Marker} from 'react-web-ar'
 
-/**
- * webcam is the default source type for analysing the marker
- */
- export default function webcam(){
+export default function webcant() {
+
+    
     return (
-      <AFrameRenderer inherent={true}>
-        <Marker parameters={{ preset: "hiro" }}>
-          <a-box color="blue" position="0 0.09 0" scale="0.4 0.8 0.8">
-            <a-animation
-              attribute="rotation"
-              to="360 0 0"
-              dur="2000"
-              easing="linear"
-              repeat="indefinite"
-            />
-          </a-box>
-        </Marker>
-      </AFrameRenderer>
-    )
-  }
+        <AFrameRenderer arToolKit={{ sourceType: 'webcam' }} >
+            <Marker parameters={{ preset: 'hiro' }}>
+
+                <a-box color="blue" position="0 0.09 0" scale="0.4 0.8 0.8">
+                    <a-animation
+                        attribute="rotation"
+                        to="360 0 0"
+                        dur="2000"
+                        easing="linear"
+                        repeat="indefinite"
+                    />
+                </a-box>
